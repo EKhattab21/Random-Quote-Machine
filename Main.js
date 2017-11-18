3eb752f235bc075d275b9f2c3ea734772efccaf3
Eslam ehab <eslamkhattab21@gmail.com>
2017-11-18 21:54:38 +0200

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,36 @@
+﻿$(document).ready(function () {
+    var quote;
+    var author;
+    function getNewQuotes() {
+        $.ajax({
+            url: 'https://api.forismatic.com/api/1.0/',
+            jsonp: 'jsonp',
+            dataType:'jsonp',
+            data: {
+                method: 'getQuote',
+                lang: 'en',
+                format: 'jsonp'
+            },
+            success: function (response) {
+                quote = response.quoteText;
+                author = response.quoteAuthor;
+                $('#quote').text(quote);
+                $('#author').text(author);
+                if (author) {
+                    $('#author').text('Said By: ' + author)
+                } else {
+                    $('#author').text('- Unkown')
+                }
+            }
+        });
+    }
+    getNewQuotes();
+    $('.change-quote').on('click', function (event) {
+        event.preventDefault();
+        getNewQuotes();
+    });
+    $('.share-quote').on('click', function (event) {
+        event.preventDefault();
+        window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent(quote + ' -- ' + author));
+    });
+});//End of jQuery
